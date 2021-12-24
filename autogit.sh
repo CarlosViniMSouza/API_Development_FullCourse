2873e43e6d65bf48af8051949f20bc98347fe6f6
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-12-23 22:54:36 -0300

    autogit.sh -> alt

--- autogit.sh
+++ autogit.sh
@@ -1,8 +1,5 @@
-git init
 git add -A
-git commit -m "Course started"
-git branch -M master
-git remote add origin https://github.com/CarlosViniMSouza/API_Development_FullCourse.git
-git push -u origin master
+git commit -m "autogit.sh -> alt"
+git push
 git status
 clear
