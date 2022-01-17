22b2954152548630f33450b6182e1ed4a275d624
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-01-17 10:35:27 -0300

    test_users.py -> add & finished

--- autogit.sh
+++ autogit.sh
@@ -1,5 +1,5 @@
 git add -A
-git commit -m "test_users.py -> add | test_create_user function -> finished"
+git commit -m "test_users.py -> add & finished"
 git push
 git status
 clear
--- tests/test_users.py
+++ tests/test_users.py
@@ -2,10 +2,11 @@ import pytest
 import jwt
 from code import schemas
 
-from main.config import settings
+from code.config import settings
 
 
 def test_create_user(client):
+
     res = client.post(
         "/users/", json={"email": "user123@gmail.com", "password": "pwd12345!@#"}
     )
@@ -15,3 +16,32 @@ def test_create_user(client):
     assert res.status_code == 201
 
 
+def test_login_user(test_user, client):
+
+    res = client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
+
+    login_res = schemas.Token(**res.json())
+
+    payload = jwt.decode(login_res.access_token,
+                         settings.secret_key,
+                         algorithms=[settings.algorithm])
+
+    id = payload.get("user_id")
+
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
+
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('sanjeev@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('sanjeev@gmail.com', None, 422)])
+def test_incorrect_login(test_user, client, email, password, status_code):
+
+    res = client.post(
+        "/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
